0b6465e1a04f42fbed69606bc48e2f112c315e88
abana <neroabana@gmail.com>
2021-04-19 14:55:12 +0100

    changed prop types in record table

--- src/components/RecordTable.js
+++ src/components/RecordTable.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { PropTypes } from "prop-types";
+import {PropTypes} from "prop-types";
 import "../styles/RecordTable.css";
 
 const RecordTable = ({ data }) => {
