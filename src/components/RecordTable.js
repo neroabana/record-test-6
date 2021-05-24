29d67616bc3cce225f1c708131b8a883e732082e
abana <neroabana@gmail.com>
2021-05-24 05:44:55 +0100

    data prop for record Table

--- src/components/RecordTable.js
+++ src/components/RecordTable.js
@@ -2,7 +2,7 @@ import React from "react";
 import {PropTypes} from "prop-types";
 import "../styles/RecordTable.css";
 
-const RecordTable = ({ data }) => {
+const RecordTable = ({data}) => {
   return (
     <>
       <table>
