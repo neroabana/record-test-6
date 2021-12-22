c876fa5a6cb215231917a7c348074ec824088ef2
Ali Abana <neroabana@gmail.com>
2021-12-22 17:14:47 +0100

    terse

--- src/test/app.test.js
+++ src/test/app.test.js
@@ -3,7 +3,6 @@ import {render} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Filter from "../components/Filter";
 import RecordTable from "../components/RecordTable";
-
 describe("The rendition of <Filter>", () => {  
   test("if the 'Sort by name' label renders correctly", () => {
     const { getByText } = render(<Filter />);
